6eddbaef10e9e451dcdeebed6b5c1e20dd847dd3
tqnghia1998 <41647635+tqnghia1998@users.noreply.github.com>
2020-03-08 00:36:37 +0700

    testing ant design

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'antd-mobile-rn';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button>antd-mobile-rn button</Button>
     </View>
   );
 }
